8d9100e0e73e72a5d94b1acfe3f68da985f6755a
naimulhassan <developernaimul00@gmail.com>
2024-07-18 11:05:00 +0600

    init

---
+++ android/app/src/main/kotlin/com/example/paintpal/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.paintpal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/screen/test_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../common_widgets/button/common_button.dart';
+import '../common_widgets/other_widgets/common_loader.dart';
+
+
+class TestScreen extends StatefulWidget {
+  const TestScreen({super.key});
+
+  @override
+  State<TestScreen> createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: CommonButton(
+                titleText: "titleText",
+                onTap: () {
+                  const CommonLoader();
+                  setState(() {});
+                })));
+  }
+}
