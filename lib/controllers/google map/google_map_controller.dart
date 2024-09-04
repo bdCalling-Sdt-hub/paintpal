73a6da8535125e0531634603d8f0d03fceb18606
naimulhassan <developernaimul00@gmail.com>
2024-09-04 15:41:21 +0600

    improve code

--- lib/controllers/google map/google_map_controller.dart
+++ lib/controllers/google map/google_map_controller.dart
@@ -16,11 +16,18 @@ class ShowGoogleMapController extends GetxController {
   num latitude = 0;
   num longitude = 0;
 
+  Position? positions;
+
   List selectedOption = ["Paint Shop", "Remove Paint"];
 
-  String selectRole = "User";
+  String selectRole = "Paint Shop";
 
   setSelectedRole(value) {
+    if (value == "Paint Shop") {
+      fetchNearbyPlaces();
+    } else {
+      fetchNearbyRemovePaint();
+    }
     selectRole = value;
     update();
   }
@@ -43,19 +50,19 @@ class ShowGoogleMapController extends GetxController {
   }
 
   getCurrentLocation() async {
-    Position? positions = await LocationService.getCurrentPosition();
+    positions = await LocationService.getCurrentPosition();
     if (positions != null) {
-      latitude = positions.latitude;
-      longitude = positions.longitude;
+      latitude = positions!.latitude;
+      longitude = positions!.longitude;
 
       kGooglePlex = CameraPosition(
-        target: LatLng(positions.latitude, positions.longitude),
+        target: LatLng(positions!.latitude, positions!.longitude),
         zoom: 14,
       );
       CameraPosition newCameraPosition = CameraPosition(
-          target: LatLng(positions.latitude, positions.longitude), zoom: 14);
+          target: LatLng(positions!.latitude, positions!.longitude), zoom: 14);
 
-      fetchNearbyRemovePaint(positions);
+      fetchNearbyPlaces();
       final GoogleMapController googleMapController = await controller.future;
       await googleMapController
           .animateCamera(CameraUpdate.newCameraPosition(newCameraPosition));
@@ -63,57 +70,10 @@ class ShowGoogleMapController extends GetxController {
     }
   }
 
-  // Future<void> fetchNearbyPlaces(positions) async {
-  //   var headers = {
-  //     'Content-Type': 'application/json',
-  //     'X-Goog-Api-Key': 'AIzaSyBT1HkkjBVBLJVm0pWHdj6WcG_gnUmaoaE',
-  //     'X-Goog-FieldMask':
-  //         'places.displayName,places.formattedAddress,places.location'
-  //   };
-  //   var request = http.Request('POST',
-  //       Uri.parse('https://places.googleapis.com/v1/places:searchNearby'));
-  //   request.body = json.encode({
-  //     "includedTypes": ["hardware_store"],
-  //     "maxResultCount": 10,
-  //     "locationRestriction": {
-  //       "circle": {
-  //         "center": {
-  //           "latitude": positions.latitude,
-  //           "longitude": positions.longitude
-  //         },
-  //         "radius": 1000
-  //       }
-  //     }
-  //   });
-  //   request.headers.addAll(headers);
-  //
-  //   http.StreamedResponse response = await request.send();
-  //
-  //   if (response.statusCode == 200) {
-  //     var data = await response.stream.bytesToString();
-  //
-  //     List places = jsonDecode(data)["places"] ?? [];
-  //
-  //     for (int i = 0; i < places.length; i++) {
-  //       PlaceModel placeModel = PlaceModel.fromJson(places[i]);
-  //
-  //       Marker newMarker = Marker(
-  //           markerId: MarkerId("${marker.length}"),
-  //           infoWindow: InfoWindow(
-  //             title: placeModel.displayName.text,
-  //             onTap: () => MakePayment.makePaymentSheet(placeModel),
-  //           ),
-  //           position: LatLng(
-  //               placeModel.location.latitude, placeModel.location.longitude));
-  //
-  //       marker.add(newMarker);
-  //     }
-  //
-  //     update();
-  //   } else {}
-  // }
+  Future<void> fetchNearbyPlaces() async {
+    if (positions == null) return;
 
-  Future<void> fetchNearbyRemovePaint(positions) async {
+    print("fdsjfdlkfj;l");
     var headers = {
       'Content-Type': 'application/json',
       'X-Goog-Api-Key': 'AIzaSyBT1HkkjBVBLJVm0pWHdj6WcG_gnUmaoaE',
@@ -123,15 +83,15 @@ class ShowGoogleMapController extends GetxController {
     var request = http.Request('POST',
         Uri.parse('https://places.googleapis.com/v1/places:searchNearby'));
     request.body = json.encode({
-      "includedTypes": ["waste_management"],
+      "includedTypes": ["hardware_store"],
       "maxResultCount": 10,
       "locationRestriction": {
         "circle": {
           "center": {
-            "latitude": positions.latitude,
-            "longitude": positions.longitude
+            "latitude": positions!.latitude,
+            "longitude": positions!.longitude
           },
-          "radius": 10000
+          "radius": 1000
         }
       }
     });
@@ -143,6 +103,67 @@ class ShowGoogleMapController extends GetxController {
       var data = await response.stream.bytesToString();
 
       List places = jsonDecode(data)["places"] ?? [];
+
+      marker.clear();
+      for (int i = 0; i < places.length; i++) {
+        PlaceModel placeModel = PlaceModel.fromJson(places[i]);
+
+        Marker newMarker = Marker(
+            markerId: MarkerId("${marker.length}"),
+            infoWindow: InfoWindow(
+              title: placeModel.displayName.text,
+              onTap: () => MakePayment.makePaymentSheet(placeModel),
+            ),
+            position: LatLng(
+                placeModel.location.latitude, placeModel.location.longitude));
+
+        marker.add(newMarker);
+      }
+
+      update();
+    }
+  }
+
+  Future<void> fetchNearbyRemovePaint() async {
+    if (positions == null) return;
+
+    var headers = {
+      'Content-Type': 'application/json',
+      'X-Goog-Api-Key': 'AIzaSyBT1HkkjBVBLJVm0pWHdj6WcG_gnUmaoaE',
+      'X-Goog-FieldMask':
+          'places.displayName,places.formattedAddress,places.location'
+    };
+    var request = http.Request('POST',
+        Uri.parse('https://places.googleapis.com/v1/places:searchNearby'));
+    request.body = json.encode({
+      "includedTypes": ["Hazardous Waste Facility"],
+      // Replace with a supported type
+      "maxResultCount": 10,
+      "locationRestriction": {
+        "circle": {
+          "center": {
+            "latitude": positions!.latitude,
+            "longitude": positions!.longitude
+          },
+          "radius": 10000
+        }
+      }
+    });
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+    var data = await response.stream.bytesToString();
+
+    print(data);
+    marker.clear();
+    update();
+
+    if (response.statusCode == 200) {
+      List places = jsonDecode(data)["places"] ?? [];
+
+      marker.clear();
+
+      print("object ${places.length}");
 
       for (int i = 0; i < places.length; i++) {
         PlaceModel placeModel = PlaceModel.fromJson(places[i]);
@@ -160,6 +181,6 @@ class ShowGoogleMapController extends GetxController {
       }
 
       update();
-    } else {}
+    }
   }
 }
