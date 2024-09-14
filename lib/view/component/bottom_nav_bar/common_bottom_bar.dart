e458cbbcb5faa373cae1941d001a623a4329daac
naimulhassan <developernaimul00@gmail.com>
2024-09-14 16:30:30 +0600

    improve code

--- lib/view/component/bottom_nav_bar/common_bottom_bar.dart
+++ lib/view/component/bottom_nav_bar/common_bottom_bar.dart
@@ -2,6 +2,11 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:paintpal/view/screen/common_screen/profile/profile_screen.dart';
+import 'package:paintpal/view/screen/common_screen/setting/setting_screen.dart';
+import 'package:paintpal/view/screen/screen/Room/add_room.dart';
+import 'package:paintpal/view/screen/screen/home/home_screen.dart';
+import 'package:paintpal/view/screen/screen/location/location_screen.dart';
 import '../../../core/app_routes.dart';
 import '../../../utils/app_colors.dart';
 
@@ -15,18 +20,11 @@ class CommonBottomNavBar extends StatefulWidget {
 }
 
 class _CommonBottomNavBarState extends State<CommonBottomNavBar> {
-  var bottomNavIndex = 0;
-
-  @override
-  void initState() {
-    bottomNavIndex = widget.currentIndex;
-    super.initState();
-  }
-
-  int selectedIndex = 0;
+  static int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    selectedIndex = widget.currentIndex;
     return CurvedNavigationBar(
         index: widget.currentIndex > 5 ? 2 : widget.currentIndex,
         height: 60,
@@ -61,62 +59,62 @@ class _CommonBottomNavBarState extends State<CommonBottomNavBar> {
                   : AppColors.black),
         ]);
 
-    // return SingleChildScrollView(
-    //   physics: const NeverScrollableScrollPhysics(),
-    //   child: Container(
-    //     width: MediaQuery.of(context).size.width,
-    //     alignment: Alignment.center,
-    //     padding: EdgeInsets.all(12.sp),
-    //     decoration: BoxDecoration(
-    //         color: AppColors.blueLight,
-    //         borderRadius: BorderRadius.only(
-    //             topLeft: Radius.circular(20.r),
-    //             topRight: Radius.circular(20.r))),
-    //     child: Row(
-    //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //       children: List.generate(unselectedIcons.length, (index) {
-    //         return GestureDetector(
-    //           onTap: () => onTap(index),
-    //           child: Container(
-    //             margin: EdgeInsetsDirectional.all(12.sp),
-    //             child: Column(
-    //               children: [
-    //                 index == bottomNavIndex
-    //                     ? selectedIcons[index]
-    //                     : unselectedIcons[index],
-    //               ],
-    //             ),
-    //           ),
-    //         );
-    //       }),
-    //     ),
-    //   ),
-    // );
   }
 
   void onTap(int index) async {
-    if (kDebugMode) {
-      print(widget.currentIndex);
+    bool animation = false;
+
+    if (index < widget.currentIndex) {
+      animation = true;
     }
     if (index == 0) {
       if (!(widget.currentIndex == 0)) {
-        Get.toNamed(AppRoutes.home);
+        Get.to(
+          const HomeScreen(),
+          transition: animation ? Transition.leftToRight : Transition.rightToLeft ,
+          duration: const Duration(
+              milliseconds: 600),
+        );
       }
     } else if (index == 1) {
       if (!(widget.currentIndex == 1)) {
-        Get.toNamed(AppRoutes.location);
+        Get.to(
+          const LocationScreen(),
+          transition: animation ? Transition.leftToRight : Transition.rightToLeft ,
+          duration: const Duration(
+              milliseconds: 600),
+        );
+        // Get.toNamed(AppRoutes.location);
       }
     } else if (index == 2) {
       if (!(widget.currentIndex == 2)) {
-        Get.toNamed(AppRoutes.addRoom);
+        Get.to(
+          const AddRoom(),
+          transition: animation ? Transition.leftToRight : Transition.rightToLeft ,
+          duration: const Duration(
+              milliseconds: 600),
+        );
+        // Get.toNamed(AppRoutes.addRoom);
       }
     } else if (index == 3) {
       if (!(widget.currentIndex == 3)) {
-        Get.toNamed(AppRoutes.setting);
+        Get.to(
+          const SettingScreen(),
+          transition: animation ? Transition.leftToRight : Transition.rightToLeft ,
+          duration: const Duration(
+              milliseconds: 600),
+        );
+        // Get.toNamed(AppRoutes.setting);
       }
     } else if (index == 4) {
       if (!(widget.currentIndex == 4)) {
-        Get.toNamed(AppRoutes.profile);
+        Get.to(
+          const ProfileScreen(),
+          transition: animation ? Transition.leftToRight : Transition.rightToLeft ,
+          duration: const Duration(
+              milliseconds: 600),
+        );
+        // Get.toNamed(AppRoutes.profile);
       }
     }
   }
